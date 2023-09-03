654113d1dcc81826057d5069c591768724607f85
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-09-03 12:48:17 +0530

    title 🔗

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <nav className="flex w-full items-center justify-between p-4 px-8 h-[60px]">
       <h1 className="text-2xl font-bold bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent">
-        RemindMe
+        Task Tracker
       </h1>
       <div className="flex gap-4 items-center">
         <UserButton afterSignOutUrl="/" />
