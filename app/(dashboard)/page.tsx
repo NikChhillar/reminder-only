97b01bfc62dc15b24b9aba14b7764ab2c77da4eb
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-09-06 15:53:43 +0530

    added loading skeletons

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { Skeleton } from "@/components/ui/skeleton";
+import { currentUser } from "@clerk/nextjs";
+import { Suspense } from "react";
 
-const DashboardPage = () => {
-    return (
-        <div>DashboardPage</div>
-    )
+async function WelcomMsg() {
+  const user = await currentUser();
+
+  if (!user) {
+    return <div>error</div>;
+  }
+
+  return (
+    <div className="flex w-full mb-12">
+      <h1 className="text-4xl font-bold">
+        Welcome, <br /> {user.firstName} {user.lastName}
+      </h1>
+    </div>
+  );
 }
 
-export default DashboardPage
+function WelcomeMsgFallBack() {
+  return (
+    <div className="flex w-full mb-12">
+      <h1 className="text-4xl font-bold">
+        <Skeleton className="w-[180px] h-[36px]" />
+        <Skeleton className="w-[250px] h-[36px]" />
+      </h1>
+    </div>
+  );
+}
+
+const DashboardPage = async () => {
+  return (
+    <>
+      <Suspense fallback={<WelcomeMsgFallBack />}>
+        <WelcomMsg />
+      </Suspense>
+    </>
+  );
+};
+
+export default DashboardPage;
