22b398eccc53cb313ba1f18ac1139c45c0f15cef
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-09-06 16:10:36 +0530

    added colors

---
+++ lib/constants.ts
@@ -0,0 +1,19 @@
+export enum CollectionColors {
+  "Dragon's Fury" = "bg-gradient-to-r from-red-500 to-orange-400",
+  "Swordmaster's Resolve" = "bg-gradient-to-r from-gray-400 to-slate-600",
+  "Mystic Portal" = "bg-gradient-to-r from-purple-400 to-indigo-600",
+  "Adventure Quest" = "bg-gradient-to-r from-green-500 to-teal-400",
+  "Enchanted Blade" = "bg-gradient-to-r from-blue-300 to-cyan-600",
+  "Elemental Power" = "bg-gradient-to-r from-yellow-500 to-red-400",
+  "Isekai Odyssey" = "bg-gradient-to-r from-indigo-400 to-violet-600",
+  "Warrior's Valor" = "bg-gradient-to-r from-maroon-400 to-gold-600",
+  "Epic Battle" = "bg-gradient-to-r from-red-600 to-crimson-800",
+  "Magic Mirage" = "bg-gradient-to-r from-teal-400 to-blue-600",
+  "Legendary Quest" = "bg-gradient-to-r from-green-400 to-lime-600",
+  "Demon Slayer" = "bg-gradient-to-r from-black-600 to-red-700",
+  "Astral Adventure" = "bg-gradient-to-r from-indigo-500 to-blue-700",
+  "Sorcerer's Spark" = "bg-gradient-to-r from-violet-500 to-pink-600",
+  "Knight's Honor" = "bg-gradient-to-r from-gray-500 to-slate-700",
+}
+
+export type CollectionColor = keyof typeof CollectionColors;
