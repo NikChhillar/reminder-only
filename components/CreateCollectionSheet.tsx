18ae5c3bafa3fd41b1714481fb15a06b23f959c4
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-09-07 10:30:18 +0530

    sidebar for collection creation

--- components/CreateCollectionSheet.tsx
+++ components/CreateCollectionSheet.tsx
@@ -1,4 +1,34 @@
+import {
+  createCollectionSchema,
+  createCollectionSchemaType,
+} from "@/schema/createCollection";
 import { Sheet, SheetContent, SheetDescription, SheetHeader } from "./ui/sheet";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { toast } from "./ui/use-toast";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { cn } from "@/lib/utils";
+import { CollectionColors, CollectionColor } from "@/lib/constants";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
+import { Loader2 } from "lucide-react";
 
 interface Props {
   open: boolean;
@@ -6,8 +36,39 @@ interface Props {
 }
 
 export default function CreateCollectionSheet({ open, onOpenChange }: Props) {
+  const form = useForm<createCollectionSchemaType>({
+    resolver: zodResolver(createCollectionSchema),
+    defaultValues: {},
+  });
+
+  const router = useRouter();
+
+  const onSubmit = async (data: createCollectionSchemaType) => {
+    try {
+      openChangeWrapper(false);
+      router.refresh();
+
+      toast({
+        title: "Success",
+        description: "Collection created successfully!",
+      });
+    } catch (e) {
+      toast({
+        title: "Error",
+        description: "Something went wrong. Please try again later",
+        variant: "destructive",
+      });
+      console.log("Error while creating collection", e);
+    }
+  };
+
+  const openChangeWrapper = (open: boolean) => {
+    form.reset();
+    onOpenChange(open);
+  };
+
   return (
-    <Sheet open={open} onOpenChange={onOpenChange}>
+    <Sheet open={open} onOpenChange={openChangeWrapper}>
       <SheetContent>
         <SheetHeader>
           <SheetHeader>Add new collection...</SheetHeader>
@@ -16,6 +77,85 @@ export default function CreateCollectionSheet({ open, onOpenChange }: Props) {
             collection...
           </SheetDescription>
         </SheetHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Personal" {...field} />
+                  </FormControl>
+                  <FormDescription>Collection name</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="color"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Color</FormLabel>
+                  <FormControl>
+                    <Select onValueChange={(color) => field.onChange(color)}>
+                      <SelectTrigger
+                        className={cn(
+                          "w-full h-8 text-white",
+                          CollectionColors[field.value as CollectionColor]
+                        )}
+                      >
+                        <SelectValue
+                          placeholder="Color"
+                          className="w-full h-8"
+                        />
+                      </SelectTrigger>
+                      <SelectContent className="w-full">
+                        {Object.keys(CollectionColors).map((color) => (
+                          <SelectItem
+                            key={color}
+                            value={color}
+                            className={cn(
+                              `w-full h-8 rounded-md my-1 text-white focus:text-white focus:font-bold focus:ring-2 ring-neutral-600 focus:ring-inset dark:focus:ring-white focus:px-8`,
+                              CollectionColors[color as CollectionColor]
+                            )}
+                          >
+                            {color}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                  <FormDescription>
+                    Select a color for your collection
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+
+        <div className="flex flex-col gap-3 mt-4">
+          <Separator />
+          <Button
+            disabled={form.formState.isSubmitting}
+            variant={"outline"}
+            className={cn(
+              form.watch("color") &&
+                CollectionColors[form.getValues("color") as CollectionColor]
+            )}
+            onClick={form.handleSubmit(onSubmit)}
+          >
+            Confirm
+            {form.formState.isSubmitting && (
+              <Loader2 className="ml-2 h-4 w-4 animate-spin" />
+            )}
+          </Button>
+        </div>
       </SheetContent>
     </Sheet>
   );
